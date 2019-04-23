51647d9ccb03e705980141b671e07525e749acbb
DavidZhang197 <david.zhang.197@gmail.com>
2019-04-22 20:14:02 -0400

    Base Version

--- app/src/main/java/com/example/rucrowded/Locations.java
+++ app/src/main/java/com/example/rucrowded/Locations.java
@@ -5,6 +5,10 @@ import android.os.Bundle;
 
 public class Locations extends AppCompatActivity {
 
+    private static final String url = "capstone2019.cpayr1ebu9xg.us-east-2.rds.amazonaws.com";
+    private static final String user = "administrator";
+    private static final string pass = "capstone2019";
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
