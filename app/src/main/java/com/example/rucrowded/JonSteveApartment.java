22b1b94c62332c0dd4e63e29ebf52e2687ba51b6
DavidZhang197 <david.zhang.197@gmail.com>
2019-04-22 21:03:02 -0400

    Base Version

---
+++ app/src/main/java/com/example/rucrowded/JonSteveApartment.java
@@ -0,0 +1,5 @@
+package com.example.rucrowded;
+
+public class JonSteveApartment {
+
+}
--- app/src/main/java/com/example/rucrowded/Locations.java
+++ app/src/main/java/com/example/rucrowded/Locations.java
@@ -1,17 +1,48 @@
 package com.example.rucrowded;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class Locations extends AppCompatActivity {
 
-    private static final String url = "capstone2019.cpayr1ebu9xg.us-east-2.rds.amazonaws.com";
-    private static final String user = "administrator";
-    private static final string pass = "capstone2019";
-    
+    ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_locations);
+
+        listView = (ListView) findViewById(R.id.listview_locations);
+
+        final ArrayList<String> arrayList = new ArrayList<String>();
+
+        arrayList.add("Jon and Steve's apartment");
+        arrayList.add("David's house");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList);
+
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String row = arrayList.get(position).toString();
+                Toast.makeText(Locations.this, "clicked item:" + position + " " + row, Toast.LENGTH_SHORT).show();
+
+                switch(position){
+                    case 0:
+                        Intent startIntent = new Intent(getApplicationContext(), JonSteveApartment.class);
+                        startActivity(startIntent);
+                }
+            }
+        });
     }
 }
